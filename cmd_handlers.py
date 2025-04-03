acb755d82b5d2fea64804e80825abfd29654f709
Bruno Storani <darkaraus@gmail.com>
2025-04-03 17:07:15 -0300

    Modularizacion en utilities.py

--- cmd_handlers.py
+++ cmd_handlers.py
@@ -1,6 +1,7 @@
 import connection
 from constants import *
-from os import path, listdir
+from os import listdir
+from utilities import *
 
 def quit_handler(cnn, command_parts):
     """
@@ -34,32 +35,3 @@ def file_listing_handler(cnn, command_parts):
         return
 
     return
-
-
-def check_argument_count(cnn, command_parts, expected_count):
-    """
-    Verifica si el número de argumentos es correcto.
-    """
-    if len(command_parts) != expected_count:
-        send_response(cnn, INVALID_ARGUMENTS)
-        return False
-    return True
-
-
-def check_directory_exists(cnn, directory):
-    """
-    Verifica si el directorio existe.
-    """
-    if not path.exists(directory):
-        send_response(cnn, INTERNAL_ERROR)
-        return False
-    return True
-
-
-def send_response(cnn, code):
-    """
-    Respuesta modularizada.
-    """
-    cnn.socket.send(
-        (f"{code} {error_messages[code]}{EOL}").encode()
-        )
